377fa913be9d4fe9f790890c79ed9bc8891f5bba
infoomics <sbx_94@hotmail.com>
2023-02-15 13:47:49 +0000

    restyle

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,5 @@
-exports.handle500Error = (err, req, res, nxt) => {
+exports.handle500Error = (err, req, res, next) => {
   console.log(err);
+  
   res.status(500).send({ msg: "Soz we made a mistake" });
 };
